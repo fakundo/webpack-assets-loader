de3c10b842c347aec1d45c3e14466e429aca1190
fakundo <fakundo.roman@gmail.com>
2017-06-22 17:40:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Webpack Assets Loader
+
+1. Copies files to some output directory
+2. Copied files get hashed names
+3. Returns hashed filename
---
+++ index.js
@@ -0,0 +1,35 @@
+var loaderUtils = require('loader-utils');
+var fs = require('fs-extra');
+var path = require('path');
+var fnv = require('fnv');
+
+function createToken(request) {
+  var h = new fnv.FNV();
+  h.update(Buffer(request));
+  return h.digest('hex');
+}
+
+/**
+ * @todo Add require hook for SSR
+ */
+
+module.exports = function () {};
+
+module.exports.pitch = function (remainingRequest) {
+  if (this.cacheable) this.cacheable();
+  this.addDependency(remainingRequest);
+
+  var callback = this.async();
+  var extension = path.extname(remainingRequest);
+  var options = loaderUtils.getOptions(this);
+  var token = createToken(remainingRequest);
+  var nextName = token + extension;
+
+  fs.copy(
+    remainingRequest,
+    path.resolve(options.outputDir, nextName),
+    function (err) {
+      callback(err, 'module.exports = ' + JSON.stringify(options.urlPath + nextName) + ';');
+    }
+  );
+};
